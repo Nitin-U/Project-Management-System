5ec77ecc3a27a397e4ce99885f2ef5f63d5f56bd
bnitin6034 <bnitin19@tbc.edu.np>
2023-05-18 16:07:53 +0545

    Implementing Laravel GATE policy to define authorization system differentiating between Project Manager and Normal Users

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\User;
+use App\Models\Team;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,21 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        /* define a admin user role */
+        Gate::define('isPM', function($user) {
+
+            return $user->role == 'Project Manager';
+ 
+         });
+
+         Gate::define('viewTeamTasks', function (User $user, Team $team) {
+            if ($user->role === 'Project Manager') {
+                return true; // Allow project managers to view team tasks
+            }
+            
+            // Check if the user is a team member of the specified team
+            return $user->teams->contains($team);
+        });
+
     }
 }
